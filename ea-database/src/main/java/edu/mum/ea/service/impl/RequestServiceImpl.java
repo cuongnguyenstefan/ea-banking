c00efa20cf485b724b20a4a35c30f4368c039f55
cuong <stefan.cuongnguyen@gmail.com>
2016-11-19 00:10:57 -0600

    update annotations

--- ea-database/src/main/java/edu/mum/ea/service/impl/RequestServiceImpl.java
+++ ea-database/src/main/java/edu/mum/ea/service/impl/RequestServiceImpl.java
@@ -1,8 +1,11 @@
 package edu.mum.ea.service.impl;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 public class RequestServiceImpl {
 
 }
