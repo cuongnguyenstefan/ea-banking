ddbdaa47237e73d1ddb90bae1a2ab217a22c7044
Bijay Regmi <er.bijayregmi@gmail.com>
2016-11-18 21:31:13 -0600

    entities

--- ea-database/src/main/java/edu/mum/ea/entity/AccountSaving.java
+++ ea-database/src/main/java/edu/mum/ea/entity/AccountSaving.java
@@ -3,7 +3,7 @@ package edu.mum.ea.entity;
 import javax.persistence.Entity;
 
 @Entity
-public class Saving extends Account {
+public class AccountSaving extends Account {
 
 	private double interest;
 
