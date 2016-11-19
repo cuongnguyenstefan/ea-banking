9811d8bfdae990f8a2bb2860da9b5a4f3fd93fee
cuong <stefan.cuongnguyen@gmail.com>
2016-11-18 20:30:39 -0600

    create serviceimpl

---
+++ ea-database/src/main/java/edu/mum/ea/service/impl/StaffServiceImpl.java
@@ -0,0 +1,10 @@
+package edu.mum.ea.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import edu.mum.ea.service.StaffService;
+
+@Service
+public class StaffServiceImpl implements StaffService {
+
+}
